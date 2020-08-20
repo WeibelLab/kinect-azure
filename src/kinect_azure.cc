01e5945834802141f1ebc0cce1ef318819547ab9
Alexander Schonken <ajschonken@gmail.com>
2020-08-20 12:13:36 -0700

    Slight changes testing context-awareness

--- src/kinect_azure.cc
+++ src/kinect_azure.cc
@@ -1294,6 +1294,7 @@ Napi::Value MethodStopListening(const Napi::CallbackInfo &info)
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
+
   exports.Set(Napi::String::New(env, "init"), Napi::Function::New(env, MethodInit));
   exports.Set(Napi::String::New(env, "getInstalledCount"), Napi::Function::New(env, MethodGetInstalledCount));
   exports.Set(Napi::String::New(env, "getSerialNumber"), Napi::Function::New(env, MethodGetSerialNumber));
